965e91f8a78f138d327d9f64f821508df55249d5
Aishwarya Saravanan <aishwarya.saravanan@ibm.com>
2021-03-10 19:59:56 +1200

    integrating firebase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
-  
 import React from 'react';
-
-import HomePage from './homepage.component';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import {Link} from 'react-router-dom';
+import HomePage from './homepage.component';
+import SignIn from '././component/sign-in/sign-in.component';
+import {auth} from './firebase/firebase.utils';
+  import Header from './component/header/header.component';
+ 
 
+const MenuPage =() =>(
+  <div>
+    <ul>
+      <li>
+        <Link to='/signIn'>SignIn</Link>
+      </li>
+    </ul>
+  </div>
+)
 class App extends React.Component {
-  render() {
+   constructor(){
+     super()
+     this.state={
+       currentUser: null
+     }
+   }
+
+   unsubscribeFromAuth = null;
+
+   componentDidMount(){
+     this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
+       this.setState({ currentUser: user});
+
+       console.log('p',user);
+     })
+   }
+
+ componentWillUnmount(){
+   this.unsubscribeFromAuth();
+ }
+  render(){
     return (
-      <div className='App'>
-        <HomePage />
+      <div>
+        <Header currentUser = {this.state.currentUser}/>
+        <Switch>
+        <Route exact path='/' component={MenuPage} />
+          <Route exact path='/home' component={HomePage} />
+          <Route path='/signin' component={SignIn} />
+        </Switch>
       </div>
     );
+
   }
 }
 
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const config = {
+        apiKey: "AIzaSyDAXZOW1kFoxWqabNw6iaR-DODyNfME47k",
+        authDomain: "ibmdemo-e4946.firebaseapp.com",
+        projectId: "ibmdemo-e4946",
+        storageBucket: "ibmdemo-e4946.appspot.com",
+        messagingSenderId: "9956866674",
+        appId: "1:9956866674:web:61d0f795ce5e5b851260eb",
+        measurementId: "G-4FJX8RH1VD"
+}
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+provider.setCustomParameters({ prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+
+export default firebase;
